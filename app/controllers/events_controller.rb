2ba8b9e23f6ddc587bd87b771231122d21169785
zal-lex <leshik89@gmail.com>
2020-02-14 16:40:07 +0300

    Added separation of user rights

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,7 @@
 class EventsController < ApplicationController
-  before_action :set_event, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:show, :index]
+  before_action :set_event, only: [:show]
+  before_action :set_current_user_event, only: [:edit, :update, :destroy]
 
   # GET /events
   def index
@@ -12,7 +14,7 @@ class EventsController < ApplicationController
 
   # GET /events/new
   def new
-    @event = Event.new
+    @event = current_user.events.build
   end
 
   # GET /events/1/edit
@@ -21,7 +23,7 @@ class EventsController < ApplicationController
 
   # POST /events
   def create
-    @event = Event.new(event_params)
+    @event = current_user.events.build(event_params)
 
     if @event.save
       redirect_to @event, notice: 'Event was successfully created.'
@@ -46,6 +48,11 @@ class EventsController < ApplicationController
   end
 
   private
+
+    def set_current_user_event
+      @event = current_user.events.find(params[:id])
+    end
+
     def set_event
       @event = Event.find(params[:id])
     end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+	def user_avatar(user)
+		# TODO: user real avatar
+		asset_path('user.png')
+	end
 end
