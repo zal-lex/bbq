fc24b5a181d21c1ebcc02f16bc9ca420477dd2d1
zal-lex <leshik89@gmail.com>
2020-02-21 22:48:06 +0300

    Avatar field added to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200221175538) do
+ActiveRecord::Schema.define(version: 20200221194512) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20200221175538) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
