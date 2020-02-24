01fa93adb2cb0dac33c9d8e74a92b7619bf228d5
zal-lex <leshik89@gmail.com>
2020-02-24 20:00:29 +0300

    Using lambdas in validation

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -8,18 +8,18 @@ class Subscription < ActiveRecord::Base
   # Проверки user_name и user_email выполняются,
   # только если user не задан
   # То есть для анонимных пользователей
-  validates :user_name, presence: true, unless: 'user.present?'
+  validates :user_name, presence: true, unless: -> { user.present? }
   validates :user_email, presence: true, 
-    format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/, unless: 'user.present?'
+    format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/, unless: -> { user.present? }
 
   # Для конкретного event_id один юзер может подписаться только один раз (если юзер задан)
-  validates :user, uniqueness: {scope: :event_id}, if: 'user.present?'
+  validates :user, uniqueness: {scope: :event_id}, if: -> { user.present? }
 
   # Или один email может использоваться только один раз (если анонимная подписка)
-  validates :user_email, uniqueness: {scope: :event_id}, unless: 'user.present?'
+  validates :user_email, uniqueness: {scope: :event_id}, unless: -> { user.present? }
 
-  validate :user_email_presence, unless: 'user.present?'
-  validate :user_event_owner, if: 'user.present?'
+  validate :user_email_presence, unless: -> { user.present? }
+  validate :user_event_owner, if: -> { user.present? }
 
   # Если есть юзер, выдаем его имя,
   # если нет – дергаем исходный метод
