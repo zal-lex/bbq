0a59e11100b172cafefffcc3c227d422f3c991da
zal-lex <leshik89@gmail.com>
2020-02-28 00:34:26 +0300

    Heroku sendgrid options setted up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,25 +77,28 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = {host: 'zal-lex-bbq.herokuapp.com'}
+  config.action_mailer.default_url_options = { host:'https://zal-lex-bbq.herokuapp.com' }
 
   # Ошибки рассылки юзеру не показываем
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Делать рассылку писем (при false приложение только имитирует отправку)
   config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.default :charset => "utf-8"
 
   # Устанавливаем протокол, по которому отправлять (SMTP)
   config.action_mailer.delivery_method = :smtp
 
   # А это для SendGrid
   ActionMailer::Base.smtp_settings = {
+    :from           => 'notify@zal-lex-bbq.app',
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'https://zal-lex-bbq.herokuapp.com',
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
 end
