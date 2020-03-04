3d6fe92edbfe032e9ffd76fadf178b1887d0daf1
zal-lex <leshik89@gmail.com>
2020-03-04 23:59:08 +0300

    Mailgun configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,7 @@ Rails.application.configure do
     address: ENV['MAILGUN_SMTP_SERVER'],
     user_name: ENV['MAILGUN_SMTP_LOGIN'],
     password: ENV['MAILGUN_SMTP_PASSWORD'],
-    domain: 'zal-lex-bbq.heroku.com',
+    domain: 'zal-lex-bbq.herokuapp.com',
     authentication: :plain
   }
 end
