3b6cea6963a452bd7a842ea33adfc374d4607365
zal-lex <leshik89@gmail.com>
2020-03-04 23:28:42 +0300

    Mailgun configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,12 +90,11 @@ Rails.application.configure do
 
   # А это для SendGrid
   ActionMailer::Base.smtp_settings = {
-    address: 'smtp.sendgrid.net',
-    port: '587',
-    authentication: 'plain',
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
-    domain: 'heroku.com',
-    enable_starttls_auto: true
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'zal-lex-bbq.heroku.com',
+    :authentication => :plain
   }
 end
