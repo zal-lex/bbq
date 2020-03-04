2647d06ed626752047658a7028934beb0948e924
zal-lex <leshik89@gmail.com>
2020-03-04 23:58:09 +0300

    Mailgun configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,11 +90,11 @@ Rails.application.configure do
 
   # А это для SendGrid
   ActionMailer::Base.smtp_settings = {
-    :port           => ENV['MAILGUN_SMTP_PORT'],
-    :address        => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'zal-lex-bbq.heroku.com',
-    :authentication => :plain
+    port: ENV['MAILGUN_SMTP_PORT'],
+    address: ENV['MAILGUN_SMTP_SERVER'],
+    user_name: ENV['MAILGUN_SMTP_LOGIN'],
+    password: ENV['MAILGUN_SMTP_PASSWORD'],
+    domain: 'zal-lex-bbq.heroku.com',
+    authentication: :plain
   }
 end
