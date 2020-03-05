d97b44d02f9a58cc7d9268ee4ad9a6f871afeccc
zal-lex <leshik89@gmail.com>
2020-03-05 22:13:58 +0300

    Delete notification for author of comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -47,8 +47,9 @@ class CommentsController < ApplicationController
   end
 
   def notify_subscribers(event, comment)
+    author_email = current_user.email unless current_user.nil?
     # Собираем всех подписчиков и автора события в массив мэйлов, исключаем повторяющиеся
-    all_emails = (event.subscriptions.map(&:user_email) + [event.user.email]).uniq
+    all_emails = (event.subscriptions.map(&:user_email) + [event.user.email] - [author_email]).uniq
 
     # По адресам из этого массива делаем рассылку
     # Как и в подписках, берём EventMailer и его метод comment с параметрами
