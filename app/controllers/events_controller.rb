d53dfa38285a51ed223d943f01bd4b7326e480e7
zal-lex <leshik89@gmail.com>
2020-03-08 15:46:27 +0300

    Scope added to event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,7 +5,7 @@ class EventsController < ApplicationController
 
   # GET /events
   def index
-    @events = Event.all
+    @events = Event.all.sorted
   end
 
   # GET /events/1
--- app/models/event.rb
+++ app/models/event.rb
@@ -12,6 +12,7 @@ class Event < ActiveRecord::Base
   validates :title, presence: true, length: {maximum: 255}
   validates :address, presence: true
   validates :datetime, presence: true
+  scope :sorted, -> { order(:datetime) }
 
   def visitors
     (subscribers + [user]).uniq
