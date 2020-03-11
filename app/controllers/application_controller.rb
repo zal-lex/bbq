1ca960c00daed6662a949c58d47def9d014faf16
zal-lex <leshik89@gmail.com>
2020-03-11 17:12:25 +0300

    Refactoring

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  YA_API = '22e1fc24-2f75-4562-bd4b-2d424a288c15'
+
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
