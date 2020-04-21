ee8607c028a14d8cfebab15a3d65d5641a33774e
zal-lex <leshik89@gmail.com>
2020-04-21 15:21:43 +0300

    Fix codestyle offenses

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,12 +3,9 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server "craio.ru", user: "deploy", roles: %w{app db web}
+server "craio.ru", user: "deploy", roles: %w[app db web]
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-
-
-
 # role-based syntax
 # ==================
 
